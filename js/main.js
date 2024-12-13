cc1d2172730426884608132ce2e5975bd2064599
jdav892 <jadavila9@gmail.com>
2024-12-13 12:59:57 -0500

    feat: animates passage and img on mouse click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 const video = document.getElementById("bg-video");
+const mainImg = document.getElementById("main-img");
+const passage = document.getElementById("passage")
 
 document.addEventListener("DOMContentLoaded", () => {
-    video.playbackRate = 1;
+    video.playbackRate = '1';
+})
+
+document.addEventListener('click', () => {
+    mainImg.style.opacity = '1';
+})
+
+document.addEventListener("scroll", () => {
+    const rect = passage.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    if(rect.top <= windowHeight * 0.8){
+        passage.style.opacity = "1";
+        passage.style.transform = "translateY(0)"
+        passage.style.transition = "opacity 0.5s ease-out, transform 0.5s ease-out";
+    }
+
 })
